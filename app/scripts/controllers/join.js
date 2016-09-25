caf61f88edc428a2344e10e6e3e1badc75fffbc9
Linse Mathew <linsemathew123@gmail.com>
2016-09-25 06:39:13 -0400

    Add scaffold.

---
+++ app/scripts/controllers/join.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spoilerZoneApp.controller:JoinCtrl
+ * @description
+ * # JoinCtrl
+ * Controller of the spoilerZoneApp
+ */
+angular.module('spoilerZoneApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
